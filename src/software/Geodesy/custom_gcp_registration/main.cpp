6cae86e7256df2ef568b34fe90dff4c2616c2ace
Seungsu Shin <dumble1@github.com>
2021-03-29 10:18:21 +0900

    fix input interface

--- src/software/Geodesy/custom_gcp_registration/main.cpp
+++ src/software/Geodesy/custom_gcp_registration/main.cpp
@@ -3,10 +3,9 @@
 int main(int argc, char **argv)
 {
     GCPRegister *gcpRegister = new GCPRegister();
-    std::string path = "/home/snatcherai/Desktop/Seungsu/prj_MVG/test/GCP/";
-    gcpRegister->openProject(path + argv[2]);
-    gcpRegister->loadGCPFile(path + argv[1]);
-    gcpRegister->saveProject(path + "test.json");
+    gcpRegister->openProject(argv[2]);
+    gcpRegister->loadGCPFile(argv[1]);
+    //gcpRegister->saveProject(path + "test.json");
     gcpRegister->registerProject();
-    gcpRegister->saveProject(path + "test_cp.bin");
+    gcpRegister->saveProject(argv[2] + ".cp");
 }
