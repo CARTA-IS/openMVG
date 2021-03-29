cd0c838145579f4ec767ae37839ab45b572298d5
Seungsu Shin <dumble1@github.com>
2021-03-29 10:47:00 +0900

    fix char* error

--- src/software/Geodesy/custom_gcp_registration/main.cpp
+++ src/software/Geodesy/custom_gcp_registration/main.cpp
@@ -3,9 +3,9 @@
 int main(int argc, char **argv)
 {
     GCPRegister *gcpRegister = new GCPRegister();
-    gcpRegister->openProject(argv[2]);
-    gcpRegister->loadGCPFile(argv[1]);
+    gcpRegister->openProject(std::string(argv[2]));
+    gcpRegister->loadGCPFile(std::string(argv[1]));
     //gcpRegister->saveProject(path + "test.json");
     gcpRegister->registerProject();
-    gcpRegister->saveProject(argv[2] + ".cp");
+    gcpRegister->saveProject(std::string(argv[2]) + ".cp");
 }
